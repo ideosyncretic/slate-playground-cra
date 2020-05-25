906f6739038915ab5caf16fa85d5d92d1932a671
Angel Coco Chen <sprazzeus@gmail.com>
2020-05-25 13:57:57 +0100

    Initialise project and install SlateJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import React dependencies.
+import React, { useEffect, useMemo, useState } from "react";
+// Import the Slate editor factory.
+import { createEditor } from "slate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  // Create a Slate editor object that won't change across renders.
+  const editor = useMemo(() => withReact(createEditor()), []);
+  return null;
+};
 
 export default App;
