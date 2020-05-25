e063f38a2cba3f84a75767d5a2dd1007f466a1a2
Angel Coco Chen <sprazzeus@gmail.com>
2020-05-25 17:06:39 +0100

    Save changes to local storage

--- src/App.js
+++ src/App.js
@@ -6,17 +6,55 @@ import { createEditor, Editor, Text, Transforms } from "slate";
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
 
+// Define our own custom set of helpers.
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.bold === true,
+      universal: true,
+    });
+
+    return !!match;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === "code",
+    });
+
+    return !!match;
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : "code" },
+      { match: (n) => Editor.isBlock(editor, n) }
+    );
+  },
+};
+
 const App = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState([
-    {
-      type: "paragraph",
-      children: [{ text: "A line of text in a paragraph." }],
-    },
-  ]);
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem("content")) || [
+      {
+        type: "paragraph",
+        children: [{ text: "A line of text in a paragraph." }],
+      },
+    ]
+  );
 
-  // Define a rendering function based on the element passed to `props`. We use
-  // `useCallback` here to memoize the function for subsequent renders.
   const renderElement = useCallback((props) => {
     switch (props.element.type) {
       case "code":
@@ -26,15 +64,40 @@ const App = () => {
     }
   }, []);
 
-  // Define a leaf rendering function that is memoized with `useCallback`.
   const renderLeaf = useCallback((props) => {
     return <Leaf {...props} />;
   }, []);
 
   return (
-    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(value) => {
+        setValue(value);
+        // Save the value to Local Storage.
+        const content = JSON.stringify(value);
+        localStorage.setItem("content", content);
+      }}
+    >
+      <div>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleBoldMark(editor);
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleCodeBlock(editor);
+          }}
+        >
+          Code Block
+        </button>
+      </div>
       <Editable
-        // Pass in the `renderElement` function.
         renderElement={renderElement}
         renderLeaf={renderLeaf}
         onKeyDown={(event) => {
@@ -42,36 +105,21 @@ const App = () => {
             return;
           }
 
+          // Replace the `onKeyDown` logic with our new commands.
           switch (event.key) {
             default: {
-              console.log(event.key);
               break;
             }
 
-            // When "`" is pressed, keep our existing code block logic.
             case "`": {
               event.preventDefault();
-              const [match] = Editor.nodes(editor, {
-                match: (n) => n.type === "code",
-              });
-              Transforms.setNodes(
-                editor,
-                { type: match ? "paragraph" : "code" },
-                { match: (n) => Editor.isBlock(editor, n) }
-              );
+              CustomEditor.toggleCodeBlock(editor);
               break;
             }
 
-            // When "B" is pressed, bold the text in the selection.
             case "b": {
               event.preventDefault();
-              Transforms.setNodes(
-                editor,
-                { bold: true },
-                // Apply it to text nodes, and split the text node up if the
-                // selection is overlapping only part of it.
-                { match: (n) => Text.isText(n), split: true }
-              );
+              CustomEditor.toggleBoldMark(editor);
               break;
             }
           }
