36dc02b574374d4ecdb6b60dff3e70c3973118c7
Angel Coco Chen <sprazzeus@gmail.com>
2020-05-25 15:05:12 +0100

    Add editor and and editable component with example event handler

--- src/App.js
+++ src/App.js
@@ -6,13 +6,41 @@ import { createEditor } from "slate";
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
 
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
   // Create a Slate editor object that won't change across renders.
   const editor = useMemo(() => withReact(createEditor()), []);
-  return null;
+
+  // Keep track of state for the value of the editor.
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ]);
+
+  // Render the Slate context.
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(newValue) => setValue(newValue)}
+    >
+      <Editable
+        // Define a new handler which prints the key that was pressed.
+        onKeyDown={(event) => {
+          if (event.key === "&") {
+            // Prevent the ampersand character from being inserted.
+            event.preventDefault();
+            // Execute the `insertText` method when the event occurs.
+            editor.insertText("and");
+          }
+        }}
+      />
+    </Slate>
+  );
 };
 
 export default App;
