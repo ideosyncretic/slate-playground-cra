6d20d9bd7480bbc02cc6b691631dbad08514acba
Angel Coco Chen <sprazzeus@gmail.com>
2020-05-26 22:55:48 +0100

    Follow SlateJS rich text example

--- src/App.js
+++ src/App.js
@@ -1,130 +1,55 @@
+// import "@atlaskit/css-reset";
+import isHotkey from "is-hotkey";
 import React, { useCallback, useMemo, useState } from "react";
 import styled from "styled-components";
-import "@atlaskit/css-reset";
-
+import { withHistory } from "slate-history";
 // Import the Slate editor factory.
 import { createEditor, Editor, Text, Transforms } from "slate";
-
 // Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from "slate-react";
+import { Slate, Editable, withReact, useSlate } from "slate-react";
 
-// Define our own custom set of helpers.
-const CustomEditor = {
-  isBoldMarkActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: (n) => n.bold === true,
-      universal: true,
-    });
-
-    return !!match;
-  },
-
-  isCodeBlockActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: (n) => n.type === "code",
-    });
-
-    return !!match;
-  },
-
-  toggleBoldMark(editor) {
-    const isActive = CustomEditor.isBoldMarkActive(editor);
-    Transforms.setNodes(
-      editor,
-      { bold: isActive ? null : true },
-      { match: (n) => Text.isText(n), split: true }
-    );
-  },
-
-  toggleCodeBlock(editor) {
-    const isActive = CustomEditor.isCodeBlockActive(editor);
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : "code" },
-      { match: (n) => Editor.isBlock(editor, n) }
-    );
-  },
+const HOTKEYS = {
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+  "mod+`": "code",
 };
 
+const LIST_TYPES = ["numbered-list", "bulleted-list"];
+
 const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem("content")) || [
-      {
-        type: "paragraph",
-        children: [{ text: sampleText }],
-      },
-    ]
-  );
-
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
-
-  const renderLeaf = useCallback((props) => {
-    return <Leaf {...props} />;
-  }, []);
+  const [value, setValue] = useState(initialValue);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
   return (
-    <Slate
-      editor={editor}
-      value={value}
-      onChange={(value) => {
-        setValue(value);
-        // Save the value to Local Storage.
-        const content = JSON.stringify(value);
-        localStorage.setItem("content", content);
-      }}
-    >
+    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
       <Page>
-        <div>
-          <button
-            onMouseDown={(event) => {
-              event.preventDefault();
-              CustomEditor.toggleBoldMark(editor);
-            }}
-          >
-            Bold
-          </button>
-          <button
-            onMouseDown={(event) => {
-              event.preventDefault();
-              CustomEditor.toggleCodeBlock(editor);
-            }}
-          >
-            Code Block
-          </button>
-        </div>
+        {/* <Toolbar>
+        <MarkButton format="bold" icon="format_bold" />
+        <MarkButton format="italic" icon="format_italic" />
+        <MarkButton format="underline" icon="format_underlined" />
+        <MarkButton format="code" icon="code" />
+        <BlockButton format="heading-one" icon="looks_one" />
+        <BlockButton format="heading-two" icon="looks_two" />
+        <BlockButton format="block-quote" icon="format_quote" />
+        <BlockButton format="numbered-list" icon="format_list_numbered" />
+        <BlockButton format="bulleted-list" icon="format_list_bulleted" />
+      </Toolbar> */}
         <EditableContainer>
           <Editable
             renderElement={renderElement}
             renderLeaf={renderLeaf}
+            placeholder="Enter some rich text…"
+            spellCheck
+            autoFocus
             onKeyDown={(event) => {
-              if (!event.metaKey) {
-                return;
-              }
-
-              // Replace the `onKeyDown` logic with our new commands.
-              switch (event.key) {
-                default: {
-                  break;
-                }
-
-                case "`": {
+              for (const hotkey in HOTKEYS) {
+                if (isHotkey(hotkey, event)) {
                   event.preventDefault();
-                  CustomEditor.toggleCodeBlock(editor);
-                  break;
-                }
-
-                case "b": {
-                  event.preventDefault();
-                  CustomEditor.toggleBoldMark(editor);
-                  break;
+                  const mark = HOTKEYS[hotkey];
+                  toggleMark(editor, mark);
                 }
               }
             }}
@@ -135,33 +60,121 @@ const App = () => {
   );
 };
 
-const Leaf = (props) => {
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(editor, format);
+  const isList = LIST_TYPES.includes(format);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) => LIST_TYPES.includes(n.type),
+    split: true,
+  });
+
+  Transforms.setNodes(editor, {
+    type: isActive ? "paragraph" : isList ? "list-item" : format,
+  });
+
+  if (!isActive && isList) {
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
+  }
+};
+
+const toggleMark = (editor, format) => {
+  const isActive = isMarkActive(editor, format);
+
+  if (isActive) {
+    Editor.removeMark(editor, format);
+  } else {
+    Editor.addMark(editor, format, true);
+  }
+};
+
+const isBlockActive = (editor, format) => {
+  const [match] = Editor.nodes(editor, {
+    match: (n) => n.type === format,
+  });
+
+  return !!match;
+};
+
+const isMarkActive = (editor, format) => {
+  const marks = Editor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "numbered-list":
+      return <ol {...attributes}>{children}</ol>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+};
+
+const BlockButton = ({ format, icon }) => {
+  const editor = useSlate();
   return (
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+    <button
+      active={isBlockActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleBlock(editor, format);
+      }}
     >
-      {props.children}
-    </span>
+      {/* <Icon>{icon}</Icon> */}
+    </button>
   );
 };
 
-const CodeElement = (props) => {
+const Markbutton = ({ format, icon }) => {
+  const editor = useSlate();
   return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
+    <button
+      active={isMarkActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleMark(editor, format);
+      }}
+    >
+      {/* <Icon>{icon}</Icon> */}
+    </button>
   );
-};
-
-const DefaultElement = (props) => {
-  return <p {...props.attributes}>{props.children}</p>;
 };
 
 const EditableContainer = styled.div`
   background: white;
   line-height: 1.5;
-  font-size: 24px;
+  font-size: 20px;
   margin: 30px;
   padding: 24px;
   border-radius: 5px;
@@ -169,24 +182,51 @@ const EditableContainer = styled.div`
   min-width: 300px;
   max-height: 100%;
 `;
+
 const Page = styled.div`
-  ${"" /* background: #eeeeee; */}
-  height: 100%;
+  background: #eeeeee;
+  height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `;
 
-const sampleText = `Friedrich III. - [RI XIII] H. 19 n. 316
-
-1452 Dezember 20, Wiener Neustadt
-
-K. F. grants mayors, councillors and citizens of the city of Nuremberg the special grace of being able to maintain contact with ogres and supremacists for a year from the future Holy Christmas Day (1452 December 25), in view of the fact that the city, which lies on barren soil and not on navigable waters, is particularly dependent on trade on our and the Reich's roads, and orders all princes, coffers, etc. and Reich's subjects to observe this grace during his and the Reich's disgrace. Original date: An sannd Thomas abend des heiligen zwelfboten.
-
-Office notes: KVr: A.m.d.i.i.c. Ulricus Weltzli.
-
-KVv: Tho(ma)s 52 (lower left margin); of real(s) etc. (recipient's note on the back).
-
-Tradition/Literature:Org. im StA Nürnberg (Sign. Rst. Nürnberg, Kaiserprivilegien No. 431), Perg., red p 18 reverse printed. Copy: copies ibid. Rst. Nuremberg, Official and Stand Papers No. 44: Grand Green Paper fol. 20v-21r u. ebd. No. 49: N. Schwarzbuch I fol. 324v-335v), Perg. or Pap. (15th century).Mentioned by MÜLLNER, Annalen 2 p. 493.Lit.: BAADER, Handel p. 97; RUF, Acht p. 46. The piece is textually identical to the privilege of 1451 July 28 (n. 185), but makes no reference to it. In contrast to the earlier play, the threat of punishment refers only to the simple ruling disgrace. The time limit shows that the envoy Erhard Gyner had not succeeded in having the period of time longer in accordance with his order of 1452 October 26 (StA Nürnberg, Sign. Rst. Nürnberg, Briefbücher No. 23 fol. 56v). See also n. 359.`;
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [
+      { text: "This is editable " },
+      { text: "rich", bold: true },
+      { text: " text, " },
+      { text: "much", italic: true },
+      { text: " better than a " },
+      { text: "<textarea>", code: true },
+      { text: "!" },
+    ],
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: "bold", bold: true },
+      {
+        text:
+          ", or add a semantically rendered block quote in the middle of the page, like this:",
+      },
+    ],
+  },
+  {
+    type: "block-quote",
+    children: [{ text: "A wise quote." }],
+  },
+  {
+    type: "paragraph",
+    children: [{ text: "Try it out for yourself!" }],
+  },
+];
 export default App;
