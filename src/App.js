f7b777fa4a74c35ba19fef13a535234ba9597a9e
Angel Coco Chen <sprazzeus@gmail.com>
2020-05-26 19:50:00 +0100

    Add some simple styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-// Import React dependencies.
 import React, { useCallback, useMemo, useState } from "react";
+import styled from "styled-components";
+import "@atlaskit/css-reset";
+
 // Import the Slate editor factory.
 import { createEditor, Editor, Text, Transforms } from "slate";
 
@@ -50,7 +52,7 @@ const App = () => {
     JSON.parse(localStorage.getItem("content")) || [
       {
         type: "paragraph",
-        children: [{ text: "A line of text in a paragraph." }],
+        children: [{ text: sampleText }],
       },
     ]
   );
@@ -79,52 +81,56 @@ const App = () => {
         localStorage.setItem("content", content);
       }}
     >
-      <div>
-        <button
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleBoldMark(editor);
-          }}
-        >
-          Bold
-        </button>
-        <button
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleCodeBlock(editor);
-          }}
-        >
-          Code Block
-        </button>
-      </div>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={(event) => {
-          if (!event.ctrlKey) {
-            return;
-          }
-
-          // Replace the `onKeyDown` logic with our new commands.
-          switch (event.key) {
-            default: {
-              break;
-            }
-
-            case "`": {
-              event.preventDefault();
-              CustomEditor.toggleCodeBlock(editor);
-              break;
-            }
-
-            case "b": {
+      <Page>
+        <div>
+          <button
+            onMouseDown={(event) => {
               event.preventDefault();
               CustomEditor.toggleBoldMark(editor);
-              break;
-            }
-          }
-        }}
-      />
+            }}
+          >
+            Bold
+          </button>
+          <button
+            onMouseDown={(event) => {
+              event.preventDefault();
+              CustomEditor.toggleCodeBlock(editor);
+            }}
+          >
+            Code Block
+          </button>
+        </div>
+        <EditableContainer>
+          <Editable
+            renderElement={renderElement}
+            renderLeaf={renderLeaf}
+            onKeyDown={(event) => {
+              if (!event.metaKey) {
+                return;
+              }
+
+              // Replace the `onKeyDown` logic with our new commands.
+              switch (event.key) {
+                default: {
+                  break;
+                }
+
+                case "`": {
+                  event.preventDefault();
+                  CustomEditor.toggleCodeBlock(editor);
+                  break;
+                }
+
+                case "b": {
+                  event.preventDefault();
+                  CustomEditor.toggleBoldMark(editor);
+                  break;
+                }
+              }
+            }}
+          />
+        </EditableContainer>
+      </Page>
     </Slate>
   );
 };
@@ -152,4 +158,35 @@ const DefaultElement = (props) => {
   return <p {...props.attributes}>{props.children}</p>;
 };
 
+const EditableContainer = styled.div`
+  background: white;
+  line-height: 1.5;
+  font-size: 24px;
+  margin: 30px;
+  padding: 24px;
+  border-radius: 5px;
+  max-width: 900px;
+  min-width: 300px;
+  max-height: 100%;
+`;
+const Page = styled.div`
+  ${"" /* background: #eeeeee; */}
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const sampleText = `Friedrich III. - [RI XIII] H. 19 n. 316
+
+1452 Dezember 20, Wiener Neustadt
+
+K. F. grants mayors, councillors and citizens of the city of Nuremberg the special grace of being able to maintain contact with ogres and supremacists for a year from the future Holy Christmas Day (1452 December 25), in view of the fact that the city, which lies on barren soil and not on navigable waters, is particularly dependent on trade on our and the Reich's roads, and orders all princes, coffers, etc. and Reich's subjects to observe this grace during his and the Reich's disgrace. Original date: An sannd Thomas abend des heiligen zwelfboten.
+
+Office notes: KVr: A.m.d.i.i.c. Ulricus Weltzli.
+
+KVv: Tho(ma)s 52 (lower left margin); of real(s) etc. (recipient's note on the back).
+
+Tradition/Literature:Org. im StA Nürnberg (Sign. Rst. Nürnberg, Kaiserprivilegien No. 431), Perg., red p 18 reverse printed. Copy: copies ibid. Rst. Nuremberg, Official and Stand Papers No. 44: Grand Green Paper fol. 20v-21r u. ebd. No. 49: N. Schwarzbuch I fol. 324v-335v), Perg. or Pap. (15th century).Mentioned by MÜLLNER, Annalen 2 p. 493.Lit.: BAADER, Handel p. 97; RUF, Acht p. 46. The piece is textually identical to the privilege of 1451 July 28 (n. 185), but makes no reference to it. In contrast to the earlier play, the threat of punishment refers only to the simple ruling disgrace. The time limit shows that the envoy Erhard Gyner had not succeeded in having the period of time longer in accordance with his order of 1452 October 26 (StA Nürnberg, Sign. Rst. Nürnberg, Briefbücher No. 23 fol. 56v). See also n. 359.`;
 export default App;
